57728debafa0f2b403f16468d3deba8c65dfd384
riogesta <riogesta.v@gmal.com>
2024-02-19 21:41:43 +0700

    add contact component
    include:
    - Contact.vue
    - ContactButton.vue

---
+++ src/components/ContactComponent/Contact.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ContactButton from "./ContactButton.vue";
+
+const props = defineProps({
+    contact: Object,
+});
+</script>
+
+<template>
+    <div class="card bg-neutral shadow-md">
+        <div class="card-body">
+            <div class="flex items-center justify-between">
+                <div class="flex-1">
+                    <h2 class="card-title">{{ contact.name }}</h2>
+                    <p>{{ contact.moto_profesional }}</p>
+                    <p class="text-sm text-gray-400">{{ contact.location }}</p>
+                    <div class="mt-3 flex gap-1">
+                        <ContactButton :links="contact.links" />
+                    </div>
+                </div>
+                <span
+                    class="relative overflow-hidden shrink-0 rounded-2xl size-28"
+                >
+                    <img
+                        src="https://media.licdn.com/dms/image/D5603AQEObO5PENOavA/profile-displayphoto-shrink_800_800/0/1694936368738?e=2147483647&v=beta&t=lr1lJPlLetWbm6WWfRsehQ-QPZPjMPx9CobO5_TH1vI"
+                        alt=""
+                    />
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/ContactComponent/ContactButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+    IconMail,
+    IconBrandGithub,
+    IconBrandLinkedin,
+} from "@tabler/icons-vue";
+
+const props = defineProps(["links"]);
+</script>
+
+<template>
+    <a
+        v-for="(link, index) in links"
+        :key="index"
+        :href="Object.values(link)"
+        target="_blank"
+        class="btn btn-outline btn-square btn-sm"
+    >
+        <i v-if="Object.keys(link) == 'email'">
+            <IconMail />
+        </i>
+        <i v-if="Object.keys(link) == 'github'">
+            <IconBrandGithub />
+        </i>
+        <i v-if="Object.keys(link) == 'linkedin'">
+            <IconBrandLinkedin />
+        </i>
+    </a>
+</template>
