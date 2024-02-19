b10355993aa50968c02715a7ecddbec52f6ed1fe
riogesta <riogesta.v@gmal.com>
2024-02-19 21:39:57 +0700

    update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 export default {
     content: ["./index.html", "./src/**/*.{vue,js}"],
     theme: {
-        fontFamily: {
-            sans: ["Gelasio", "sans-serif"],
-            mono: ["JetBrains Mono", "monospace"],
-        },
+        fontFamily: {},
         extend: {},
     },
     plugins: [require("daisyui")],
